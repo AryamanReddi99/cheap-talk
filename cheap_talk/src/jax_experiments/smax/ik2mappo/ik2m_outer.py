c0fe88209ae67e638c9f2ad014548f0820d62ebc
AryamanReddi99 <aryamanreddi@gmail.com>
2025-05-02 19:18:19 +0200

    changes

--- cheap_talk/src/jax_experiments/smax/ik2mappo/ik2m_outer.py
+++ cheap_talk/src/jax_experiments/smax/ik2mappo/ik2m_outer.py
@@ -1106,8 +1106,8 @@ def main(config):
         config = OmegaConf.to_container(config)
 
         # WANDB
-        job_type = f"K2MAPPO_K1CR_SCALED_LOSS_{config['MAP_NAME']}"
-        group = f"K2MAPPO_K1CR_SCALED_LOSS_{config['MAP_NAME']}"
+        job_type = f"iK2MAPPO_{config['MAP_NAME']}"
+        group = f"iK2MAPPO_{config['MAP_NAME']}"
         if config["USE_TIMESTAMP"]:
             group += datetime.datetime.now().strftime("_%Y-%m-%d_%H-%M-%S")
         global LOGGER
