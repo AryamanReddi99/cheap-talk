d9b859480999f313eb8c790fff273d4a6ec78a4b
AryamanReddi99 <aryamanreddi@gmail.com>
2025-05-07 15:42:35 +0200

    changes

--- cheap_talk/src/jax_experiments/smax/qmix/run_qmix_erlangen.sh
+++ cheap_talk/src/jax_experiments/smax/qmix/run_qmix_erlangen.sh
@@ -3,7 +3,7 @@
 #SBATCH -a 0 # Controls the number of replication
 #SBATCH -n 1  ## ALWAYS leave this value to 1. This is only used for MPI, which is not supported now. 
 #SBATCH -c 1
-#SBATCH -t 02:00:00
+#SBATCH -t 04:00:00
 #SBATCH --gres=gpu:a40:1
 #SBATCH -o ./logs_sbatch/%A_%a.out
 #SBATCH -e ./logs_sbatch/%A_%a.err ## Make sure to create the logs directory
