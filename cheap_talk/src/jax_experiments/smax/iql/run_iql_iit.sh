19d933a9dbd4f1a194ce9f7ba42646a7901524e5
AryamanReddi99 <aryamanreddi@gmail.com>
2025-05-09 17:40:32 +0200

    changes

---
+++ cheap_talk/src/jax_experiments/smax/iql/run_iql_iit.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#PBS -l select=1:ncpus=2:mem=2GB:ngpus=1
+#PBS -l walltime=2:00:00
+#PBS -j oe
+#PBS -N job
+#PBS -q gpu_a100
+
+cd /fastwork/prabino/tib/aryaman
+source /apps/anaconda/anaconda3/bin/activate
+module load nvidia/cuda-12.1.1
+conda activate /work/prabino/aryaman/envs/cheap
+export WANDB_API_KEY=846439c3adcf172dff32ec92ba48c80e918eee48
+
+MAP_NAME=${1}
+SEED=${2:-0}
+NUM_SEEDS=${3}
+python iql.py MAP_NAME=$MAP_NAME SEED=$SEED NUM_SEEDS=$NUM_SEEDS
