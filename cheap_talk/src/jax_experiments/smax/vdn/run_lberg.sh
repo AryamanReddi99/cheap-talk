58721cf2b12675a1ce033cd19b17ea10c41efe48
AryamanReddi99 <aryamanreddi@gmail.com>
2025-05-10 17:04:55 +0200

    changes

---
+++ cheap_talk/src/jax_experiments/smax/vdn/run_lberg.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH -J VDN
+#SBATCH -a 0 # Controls the number of replication
+#SBATCH -n 1  ## ALWAYS leave this value to 1. This is only used for MPI, which is not supported now. 
+#SBATCH -c 1
+#SBATCH -t 02:00:00
+#SBATCH --mem-per-cpu=2G
+#SBATCH --gres=gpu:a40:1
+#SBATCH -o ./logs_sbatch/%A_%a.out
+#SBATCH -e ./logs_sbatch/%A_%a.err ## Make sure to create the logs directory
+#SBATCH -A project02654                                                                                                                                                                         
+#SBATCH -o [YOUR_LOCATION]/log_sbatch/%A_%a.out
+#SBATCH -e [YOUR_LOCATION]/log_sbatch/%A_%a.err
+
+MAP_NAME=${1}
+SEED=${2:-0}
+NUM_SEEDS=${3}
+echo "Running with SEED=${SEED}"
+python vdn.py MAP_NAME=$MAP_NAME SEED=$SEED NUM_SEEDS=$NUM_SEEDS
