8c7bf0ba2c5921d10333fdf81c33e1b52618d776
AryamanReddi99 <aryamanreddi@gmail.com>
2025-05-10 17:17:32 +0200

    changes

--- cheap_talk/src/jax_experiments/smax/vdn/run_vdn_lberg.sh
+++ cheap_talk/src/jax_experiments/smax/vdn/run_vdn_lberg.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 #SBATCH -J VDN
+#SBATCH -A project02654 
 #SBATCH -a 0 # Controls the number of replication
 #SBATCH -n 1  ## ALWAYS leave this value to 1. This is only used for MPI, which is not supported now. 
 #SBATCH -c 1
-#SBATCH --mem-per-cpu 4G
-#SBATCH -t 01:30:00
-#SBATCH -p gpu
-#SBATCH --gres=gpu:1
+#SBATCH -t 02:00:00
+#SBATCH --mem-per-cpu=8G
+#SBATCH --gres=gpu:a100
 #SBATCH -o ./logs_sbatch/%A_%a.out
-#SBATCH -e ./logs_sbatch/%A_%a.err ## Make sure to create the logs directory
+#SBATCH -e ./logs_sbatch/%A_%a.err ## Make sure to create the logs directory                                                                                                                                                           
 
-MAP_NAME=$1
+
+MAP_NAME=${1}
 SEED=${2:-0}
 NUM_SEEDS=${3}
 echo "Running with SEED=${SEED}"
